872be043cd8ae76affdbf9ae96c02b0f1285a9ae
Ryan-inspires24 <kinsaryanabecklin@gmail.com>
2025-06-08 23:20:51 +0100

    Enabling CORS origins access for Vercel deployment.

--- server/server.js
+++ server/server.js
@@ -6,14 +6,18 @@ import submitRoute from './routes/formRoutes.js';
 
 const app = express();
 
-const allowedOrigins = ['http://localhost:5173', 'https://react-with-ap-is.vercel.app'];
+const allowedOrigins = ['https://react-with-ap-is.vercel.app','http://localhost:5173'];
 
 app.use(cors({
   origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) return callback(null, true);
-    return callback(new Error('Not allowed by CORS'));
-  }
+    if (!origin) return callback(null, true); 
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
 }));
 
 app.use(bodyParser.json());
